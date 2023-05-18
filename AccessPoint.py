fbf66047671343260d3fb048deb6bd39b34866ea
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2023-05-18 11:48:07 +0100

    typo

--- AccessPoint.py
+++ AccessPoint.py
@@ -1,8 +1,7 @@
-class AcessPoint:
+class AccessPoint:
 	def __init__(self, ssid, bssid, signal, band, channel):
 		self.ssid = ssid
 		self.bssid = bssid
 		self.signal = signal
 		self.band = band
 		self.channel = channel
-    
