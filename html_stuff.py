9ec236e874e316bd6587ac30fd97ded2bbe41c33
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2023-05-31 23:29:19 +0100

    Piores canais no fim

--- html_stuff.py
+++ html_stuff.py
@@ -11,7 +11,7 @@
 """
 
 from collections import OrderedDict
-
+from collections import Counter
 
 def _bssid_details_style(refresh_seconds):
 	return f"""
@@ -134,8 +134,6 @@ _ssid_details_style = f"""
 
 
 def _ssid_details_table_line(ap):
-	print("Se der erro ver se o de baixo dá um número")
-	print(ap.strength[:-1])
 	color = translate_strength_color(int(ap.strength[:-1]))
 	return f"""
 		<div class=\"{color}\"> 
@@ -236,28 +234,52 @@ _main_style = f"""
 			.rowYellow {{
 				background: rgb(255, 255, 0, 0.5);
 			}}
+			.row{{
+			}}
 		</style>
 	</head>
 """
 
 
 def translate_strength_color(strength):
+	color = ""
 	strength = int(strength)
 	if strength < 40:
-		return "rowRed"
+		color = "rowRed"
 	elif strength < 70:
-		return "rowYellow"
+		color = "rowYellow"
 	else:
-		return "rowGreen"
+		color = "rowGreen"
+	return color
+
 
 
 def _main_table_line(ap):
-	color = translate_strength_color(int(ap.strength[:-1]))
+	value = "0%"
+	if  hasattr(ap, 'strength'): 
+		color = translate_strength_color(int(ap.strength[:-1]))
+		value = int(ap.strength[:-1])
+	else:
+		ssid = ap["SSID"]
+		bssid = ap["BSSID"]
+		value = ap["Signal"]
+		radio_type = ap["Radio type"]
+		channel = ap["Channel"]
+		color = translate_strength_color(int(value[:-1]))
+		return f"""
+		<div class=\"{color}\"> 
+			<div class=\"col\"> <a href=\"/{ssid}\"> {ssid} </a> </div>
+			<div class=\"col\"> <a href=\"/{bssid}/{bssid}\"> {bssid} </a> </div>
+			<div class=\"col\"> {value} </div>
+			<div class=\"col\"> {radio_type} </div>
+			<div class=\"col\"> {channel} </div>
+		</div>
+	"""
 	return f"""
 		<div class=\"{color}\"> 
 			<div class=\"col\"> <a href=\"/{ap.ssid}\"> {ap.ssid} </a> </div>
 			<div class=\"col\"> <a href=\"/{ap.ssid}/{ap.bssidf}\"> {ap.bssid} </a> </div>
-			<div class=\"col\"> {ap.strength} </div>
+			<div class=\"col\"> {value} </div>
 			<div class=\"col\"> {ap.radio_type} </div>
 			<div class=\"col\"> {ap.channel} </div>
 		</div>
@@ -282,14 +304,16 @@ def _main_table(access_points):
 	result += " </section>\n"
 	return result
 
-
 # Count the frequency of utilization of channels
-def _count_frequency(my_list):
-	# Creating an empty dictionary
+def CountFrequency(my_list):
+ 
+    # Creating an empty dictionary
 	freq = {}
 	for item in my_list:
-		freq.setdefault(item, 0)
-		freq[item] += 1
+		if (item in freq):
+			freq[item] += 1
+		else:
+			freq[item] = 1
 	'''
 	channels = list(freq.keys())
 	channels.sort()
@@ -298,7 +322,7 @@ def _count_frequency(my_list):
 	print("---")
 	sorted_dict = {i: freq[i] for i in channels}
 	'''
-	sorted_dict = OrderedDict(sorted(freq.items()))
+	sorted_dict = OrderedDict(sorted(freq.items())) 
 	res2_4 = []
 	res_5 = []
 	for key, value in sorted_dict.items():
@@ -307,64 +331,113 @@ def _count_frequency(my_list):
 		else:
 			res_5.append({"x": int(key), "y": value})
 
-	# res = (list(sorted_dict.keys()), list(sorted_dict.values()))
-
+	#res = (list(sorted_dict.keys()), list(sorted_dict.values()))
+        
 	return res2_4, res_5
 
-
-def _main_graph_aps(access_points):
-	used_channels = map(lambda ap: ap.channel, access_points)
-	freq2_4, freq_5 = _count_frequency(used_channels)
-
+def _main_graph_APS(access_points):
+	used_channels = []
+	if  hasattr(access_points[0], 'strength'): 
+		used_channels = map(lambda ap: ap.channel,  access_points)
+	else:
+		used_channels = map(lambda ap: ap["Signal"][:-1],  access_points)
+	freq2_4, freq_5 = CountFrequency(used_channels)
+		
 	return f"""
 	<div class=\"myGraph\">
 				<div class=\"title3\">Channel utilization:</div>
 				<canvas id="myChart" style="width:100%;max-width:700px"></canvas>
 
 
-<script>
+		<script>
 
-new Chart(\"myChart\", {{
-	type: \"scatter\",
-	data: {{
-	datasets: [
-		{{
-			label: \'Freq 2.4GHz\',
+		new Chart(\"myChart\", {{
+		type: \"scatter\",
+		data: {{
+			datasets: [{{
+				label: \'Freq 2.4GHz\',
 			pointRadius: 4,
 			pointBackgroundColor: \"rgb(0,0,255)\",
 			backgroundColor: \"rgb(0,0,255)\",
 			data: {freq2_4},
-		}},
+			}},
 		{{
-			label: \'Freq 5GHz\',
+				label: \'Freq 5GHz\',
 			pointRadius: 4,
 			pointBackgroundColor: \"rgb(255,0,255)\",
 			backgroundColor: \"rgb(255,0,255)\",
 			data: {freq_5},
-		}}
-	]
-	}},
-	options: {{
-		legend: {{display: true}},
-		scales: {{
-			yAxes: [
-				{{
+			}},
+
+			]
+		}},
+		options: {{
+			legend: {{display: true}},
+
+			scales: {{
+				yAxes: [{{
 					ticks: {{
 						beginAtZero: true
 					}}
-				}}
-			]
+				}}]
+			}}
 		}}
-	}}
-}});
-</script>
+		}});
+		</script>
 
 	</div>"""
 
+"""
+============================  Opinions about AP's and channels  ============================
+"""
+
+def _more_than_one_AP_in_Channel(used_channels):
+	freq_dict = Counter(used_channels)
+	more_than_one_AP = list(filter(lambda ele: freq_dict[ele] > 1, freq_dict))
+	more_than_one_AP.sort()
+	if len(more_than_one_AP) < 1:
+		return ""
+	result = "Channels shared with two or more AP's:<ul>"
+	more_than_one_AP.sort()
+	for elem in more_than_one_AP:
+		#result += _main_table_line(elem)
+		result += f"<li>{elem}</li>"
+	result += " </ul>\n"
+	return result
+
+
+
+def _interference_Channels(used_channels):
+	channels = sorted(set(list(used_channels)))
+	channels_with_interference = list(filter(lambda channel: ((channel+1) in channels) or ((channel-1) in channels), channels)) 
+
+	if len(channels_with_interference) < 1:
+		return ""
+	result = "Channels with close neibourghs (the could interfere with each other):<ul>"
+	for elem in channels_with_interference:
+		#result += _main_table_line(elem)
+		result += f"<li>{elem}</li>"
+	result += " </ul>\n"
+	return result
+
+def _comments(access_points):
+	used_channels = []
+	if (len(access_points)) > 0:
+		if  hasattr(access_points[0], 'channel'): 
+			used_channels = list(map(lambda ap: int(ap.channel),  access_points))
+			#used_channels_copy = map(lambda ap: int(ap.channel[:-1]),  access_points)
+			
+		else:
+			used_channels = list(map(lambda ap: int(ap["Signal"][:-1]),  access_points))
+			#used_channels_copy = map(lambda ap: int(ap["Signal"][:-1]),  access_points)
+		
+	else:
+		used_channels = []
+	result = "<div  class=\"MiTable\">" + _more_than_one_AP_in_Channel(used_channels.copy()) + _interference_Channels(used_channels.copy())+ "</div>"
+	return result
 
 def _main_body(access_points):
-	return "<body><div class=\"title2\">Current Access Points near you</div>" + _main_table(
-		access_points) + _main_graph_aps(access_points) + "</body>"
+	return "<body><div class=\"title2\">Current Access Points near you</div>" + _main_table(access_points) + _main_graph_APS(access_points.copy())+ _comments(access_points) +"</body>"
 
 
 def render_main_page(available_aps):
